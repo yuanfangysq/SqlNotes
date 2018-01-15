fee42651f0eef8e9fa890b3107e607406d207ae4
yuanfangysq <ysq19941125@163.com>
2018-01-15 09:58:28 +0800

    反射

--- c# 一些方法.cs
+++ c# 一些方法.cs
@@ -15,4 +15,36 @@
             return dv.ToTable();
         }
 
+        //反射
+        [WebMethod(Description = "得到报表数据")]
+        [SoapHeader("Credentials")]
+        public DataSet GetRptData(string moduleName, DataSet dsCondition, string userNo)
+        {
+            DataSet ds = new DataSet();
+            Type type = Type.GetType("Regentsoft.Plugin.Report.Bll." + moduleName + ",Regentsoft.Plugin.Report.Bll");
+            if (type == null)
+                return ds;
+
+            object dObj = Activator.CreateInstance(type);
+            //属性
+            System.Reflection.PropertyInfo property = type.GetProperty("UserNo");
+            property.SetValue(dObj, userNo, null);
+            System.Reflection.PropertyInfo property1 = type.GetProperty("DsCondition");
+            property1.SetValue(dObj, dsCondition, null);
+
+            //获取方法的信息
+            System.Reflection.MethodInfo method = type.GetMethod("Calculate");
+            //调用方法的一些标志位，这里的含义是Public并且是实例方法，这也是默认的值
+            System.Reflection.BindingFlags flag = System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance;
+            //调用方法，用一个object接收返回值
+            object obj = method.Invoke(dObj, flag, null, null, null);
+
+            if (obj != null)
+            {
+                ds = (DataSet)obj;
+            }
+
+            return ds;
+        }
+
         
